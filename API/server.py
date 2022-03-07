761fb21b175bdd7ed7b318c5c9c3686f3451b629
Nathan Jourdain <nathan.jourdain50700@gmail.com>
2022-03-07 20:19:58 +0100

    remove token :')

--- API/server.py
+++ API/server.py
@@ -13,12 +13,16 @@ app = Quart(__name__, template_folder='templates', static_folder='static')
 
 app.secret_key = b"random bytes representing flask secret key"
 # OAuth2 must make use of HTTPS in production environment.
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true"      # !! Only in development environment.
+# !! Only in development environment.
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true"
 
-app.config["DISCORD_CLIENT_ID"] = 931334094128840734    # Discord client ID.
-app.config["DISCORD_CLIENT_SECRET"] = "fVvNIFMDQc1m3HBJ0hkTpTShgnt1nIrc"                # Discord client secret.
-app.config["DISCORD_REDIRECT_URI"] = "http://127.0.0.1:1000/api/callback"                 # URL to your callback endpoint.
-app.config["DISCORD_BOT_TOKEN"] = "OTMxMzM0MDk0MTI4ODQwNzM0.YeC6dw.zO9I_8QdNDNNZXHEkqNFbmJOjNc"                    # Required to access BOT resources.
+app.config["DISCORD_CLIENT_ID"] = 0   # Discord client ID.
+# Discord client secret.
+app.config["DISCORD_CLIENT_SECRET"] = ""
+# URL to your callback endpoint.
+app.config["DISCORD_REDIRECT_URI"] = "http://127.0.0.1:1000/api/callback"
+# Required to access BOT resources.
+app.config["DISCORD_BOT_TOKEN"] = ""
 
 # Ipc
 ipc_client = ipc.Client(
@@ -35,6 +39,7 @@ app.config['discord'] = discord
 app.register_blueprint(home, url_prefix='/')
 app.register_blueprint(api, url_prefix='/api/')
 app.register_blueprint(guilds, url_prefix='/')
+
 
 @app.errorhandler(Unauthorized)
 def redirect_unauthorized(e):
